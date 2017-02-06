89acec3e3f2fedf8457202087e36695e786157fe
bzsinger <benjamin.z.singer@gmail.com>
2017-02-05 19:26:20 -0600

    Added Collection View, Tab control, Search Bar

---
+++ MovieViewer/MovieCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieCollectionViewCell.swift
+//  MovieViewer
+//
+//  Created by Benny Singer on 1/30/17.
+//  Copyright © 2017 Benjamin Singer. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var image: UIImageView!
+    
+}
--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -9,29 +9,45 @@
 import UIKit
 import AFNetworking
 import MBProgressHUD
+import SystemConfiguration
 
-class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
 
+
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     var movies: [NSDictionary]?
+    var filteredMovies: [NSDictionary]?
+    
+    let refreshControl = UIRefreshControl()
+    
+    var selectedCell = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
         tableView.insertSubview(refreshControl, at: 0)
         
         tableView.dataSource = self
         tableView.delegate = self
         
-        MBProgressHUD.showAdded(to: self.view, animated: true)
-        getMovieData(false)
+        searchBar.delegate = self
+        
+        getMovieData(refresh: false)
 
         // Do any additional setup after loading the view.
     }
 
-    func getMovieData(_ refresh: Bool) {
+    func getMovieData(refresh: Bool) {
+        while !isInternetAvailable() {
+            print("issues")
+        }
+        
+        if (!refresh) {
+            MBProgressHUD.showAdded(to: self.view, animated: true)
+        }
+        
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -41,32 +57,19 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                 if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
                     MBProgressHUD.hide(for: self.view, animated: true)
                     self.movies = dataDictionary["results"] as! [NSDictionary]
+                    if (!refresh) { self.filteredMovies = self.movies }
                     self.tableView.reloadData()
                 }
             }
         }
-
+        if refresh {
+            refreshControl.endRefreshing()
+        }
         task.resume()
     }
     
     func refreshControlAction(_ refreshControl: UIRefreshControl) {
-        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
-            if let data = data {
-                if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
-                    MBProgressHUD.hide(for: self.view, animated: true)
-                    self.movies = dataDictionary["results"] as! [NSDictionary]
-                    self.tableView.reloadData()
-                    refreshControl.endRefreshing()
-                }
-            }
-        }
-        
-        task.resume()
-            
+        getMovieData(refresh: true)
     }
     
     override func didReceiveMemoryWarning() {
@@ -75,17 +78,16 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let movies = movies {
-            return movies.count
+        if let filteredMovies = filteredMovies {
+            return filteredMovies.count
         } else {
             return 0
         }
     }
     
-    
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
-        let movie = movies![indexPath.row]
+        let movie = filteredMovies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
         
@@ -98,19 +100,51 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         cell.overviewLabel.text = overview
         cell.posterView.setImageWith(imageUrl! as URL)
         
-        
-        print("row \(indexPath.row)")
         return cell
     }
-    
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if (searchText.isEmpty || movies == nil) {
+            filteredMovies = movies
+        } else {
+            filteredMovies = []
+            for i in 0...movies!.count - 1 {
+                let title = movies![i]["title"] as! String
+                if (title.range(of: searchText, options: .caseInsensitive) != nil) {
+                    filteredMovies?.append(movies![i])
+                }
+            }
+        }
+        tableView.reloadData()
     }
-    */
-
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func didTap(_ sender: Any) {
+        self.view.endEditing(true)
+    }
+    
+    //source: http://stackoverflow.com/questions/39558868/check-internet-connection-ios-10
+    func isInternetAvailable() -> Bool
+    {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        return (isReachable && !needsConnection)
+    }
 }
